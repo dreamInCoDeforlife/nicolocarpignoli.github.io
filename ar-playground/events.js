a3ddaf03589f9f6ab46fa38a62fa1f1429ff8bac
Aman Adhav <amanadhav@gmail.com>
2019-03-02 19:50:08 -0500

    Update events.js

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -1,16 +1,17 @@
+// register our component
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
         const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#animated-model");
 
-        // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
-                const scale = aEntity.getAttribute('scale');
+        animatedMarker.addEventListener('click', function(ev){
+            if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
+                const entity = document.querySelector('#animated-model');
+                const scale = entity.getAttribute('scale');
                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                aEntity.setAttribute('scale', scale);
+                
+                // every click, we make our model grow in size
+                entity.setAttribute('scale', scale);
             }
         });
 }});
