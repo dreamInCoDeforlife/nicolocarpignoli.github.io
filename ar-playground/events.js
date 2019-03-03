0fad53a33c30fbcf9e55c5ce9f8285ec7b2c4dea
Aman Adhav <amanadhav@gmail.com>
2019-03-02 20:37:11 -0500

    Update events.js

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -8,10 +8,7 @@ AFRAME.registerComponent('markerhandler', {
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-                const scale = aEntity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                aEntity.setAttribute('scale', scale);
-                window.open("https://www.youtube.com/");
+                     window.open("https://www.youtube.com/");
             }
         });
 }});
