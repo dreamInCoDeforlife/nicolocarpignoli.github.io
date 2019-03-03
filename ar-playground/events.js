19caed19d1791dc78f84f459ce889bd9bcd537d1
Aman Adhav <amanadhav@gmail.com>
2019-03-02 22:20:44 -0500

    Update events.js

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -2,13 +2,34 @@ AFRAME.registerComponent('markerhandler', {
 
     init: function() {
         const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#animated-model");
+        const aEntity1 = document.querySelector("#animated-model1");
+	const aEntity2 = document.querySelector("#animated-model2");
+	const aEntity3 = document.querySelector("#animated-model3");
+	const aEntity4 = document.querySelector("#animated-model4");
 
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
-                     window.open("https://www.youtube.com/");
+            if (aEntity1 && intersectedElement === aEntity1) {
+                    const entity = document.querySelector('#animated-model1');
+                const url = entity.getAttribute('url');
+                     window.open(url);
+            }
+	    if (aEntity2 && intersectedElement === aEntity2) {
+                    const entity = document.querySelector('#animated-model2');
+                const url = entity.getAttribute('url');
+                     window.open(url);
+            }
+	    if (aEntity3 && intersectedElement === aEntity3) {
+                    const entity = document.querySelector('#animated-model3');
+                const url = entity.getAttribute('url');
+                     window.open(url);
+            }
+	    if (aEntity4 && intersectedElement === aEntity4) {
+                    const entity = document.querySelector('#animated-model4');
+                const url = entity.getAttribute('url');
+                     window.open(url);
             }
         });
+
 }});
