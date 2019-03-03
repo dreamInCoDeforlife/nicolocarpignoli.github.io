676f80c4e8b2b9f71a9c2648bb551ecb2de0201e
Aman Adhav <amanadhav@gmail.com>
2019-03-02 20:10:27 -0500

    Update events.js

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -7,11 +7,7 @@ AFRAME.registerComponent('markerhandler', {
         animatedMarker.addEventListener('click', function(ev){
             if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
                 const entity = document.querySelector('#animated-model');
-                const scale = entity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                
-                // every click, we make our model grow in size
-                entity.setAttribute('scale', scale);
+                window.location = "https://www.youtube.com/";
             }
         });
 }});
