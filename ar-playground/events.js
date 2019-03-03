dfac553a9504a0dc0ebf96425500982a40d5a80d
Aman Adhav <amanadhav@gmail.com>
2019-03-02 20:29:00 -0500

    Update events.js

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -10,6 +10,7 @@ AFRAME.registerComponent('markerhandler', {
             if (aEntity && intersectedElement === aEntity) {
                 const scale = aEntity.getAttribute('scale');
                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+                aEntity.setAttribute('scale', scale);
                 window.open("https://www.youtube.com/");
             }
         });
